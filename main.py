c4211cd620a1d4dcdbce4e561a2087e0f23f42e4
Sweetsan <coderintheworldhack100@gmail.com>
2026-05-23 12:11:57 +0530

    Student Model  created

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
+from datetime import datetime
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = (
@@ -9,3 +10,16 @@ app.config["SQLALCHEMY_DATABASE_URI"] = (
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
+
+
+class Student(db.Model):
+    __tablename__ = "students"
+
+    id = db.Column(db.Integer, Primary_key=True, AutoIncrement=True)
+    full_name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    age = db.Column(db.Integer, nullable=False)
+    cgpa = db.Column(db.Float, Default=0.0)
+    is_active = db.Column(db.Boolean, Default=True)
+    joined_date = db.Column(db.Date, nullable=False)
+    created_at = db.Column(db.DateTime, Default=datetime.utcnow)
