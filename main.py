0c87012f7445043d2252e744e5262b18a6a1bbf3
Sweetsan <coderintheworldhack100@gmail.com>
2026-05-23 13:11:24 +0530

    exception handling added

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request  # import needed packages
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
 from datetime import datetime
 
+# assign the flask in object
 app = Flask(__name__)
+# MYSQL DATABASE CONNECTION CONFIGUATION CODE ADDED
 app.config["SQLALCHEMY_DATABASE_URI"] = (
     "mysql+pymysql://root:root123@localhost/api_development"
 )
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
+# asssign to db variable in SQLAlchemy(app)
 db = SQLAlchemy(app)
 
 
+# Create Student Model
 class Student(db.Model):
     __tablename__ = "students"
 
@@ -25,6 +28,7 @@ class Student(db.Model):
     created_at = db.Column(db.DateTime, Default=datetime.utcnow)
 
 
+# Create Course Model
 class Course(db.Model):
     __tablename__ = "courses"
 
@@ -35,3 +39,14 @@ class Course(db.Model):
     description = db.Column(db.Text, nullable=True)
     is_available = db.Column(db.Boolean, Default=True)
     created_at = db.Column(db.DateTime, Default=datetime.utcnow)
+
+
+if __name__ == "__Main__":
+    try:
+        with app.app_context():
+            db.session.execute(text("SELECT 1"))
+            print("SUCCESS: Your Database Connected Successfully")
+    except Exception as error:
+        print("ERROR: Your Database Connection Failed ")
+        print({error})
+    app.run(debug=True)
