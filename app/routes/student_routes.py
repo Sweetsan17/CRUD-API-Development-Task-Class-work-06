a77aa837ad2cfd27c2ffda30a4b089d74fd141ea
Sweetsan <coderintheworldhack100@gmail.com>
2026-05-28 14:28:03 +0530

    Student_routes are created and added

---
+++ app/routes/student_routes.py
@@ -0,0 +1,9 @@
+from flask import Blueprint
+from app.controllers.student_controller import *
+
+student_bp = Blueprint("students", __name__, url_prefix="/api/students")
+
+student_bp.route("", methods=["POST"])(create_student)
+student_bp.route("", methods=["GET"])(get_students)
+student_bp.route("/<int:id>", methods=["PUT"])(update_student)
+student_bp.route("/<int:id>", methods=["DELETE"])(delete_student)
