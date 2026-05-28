ffa59d4226845e489db0c07981031f90bfba997a
Sweetsan <coderintheworldhack100@gmail.com>
2026-05-28 16:20:25 +0530

    course routes are updated

--- app/routes/course_routes.py
+++ app/routes/course_routes.py
@@ -1,6 +1,10 @@
 from flask import Blueprint
-from app.controllers.course_controller import create_course
+from app.controllers.course_controller import *
 
 course_bp = Blueprint("courses", __name__, url_prefix="/api/courses")
 
 course_bp.route("", methods=["POST"])(create_course)
+course_bp.route("", methods=["GET"])(get_courses)
+course_bp.route("/<int:id>", methods=["GET"])(get_course)
+course_bp.route("/<int:id>", methods=["PUT"])(update_course)
+course_bp.route("/<int:id>", methods=["DELETE"])(delete_course)
