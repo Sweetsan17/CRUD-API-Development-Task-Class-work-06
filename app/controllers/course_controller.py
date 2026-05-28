7188073c20dacff608a68d3d467b808bcf8103dd
Sweetsan <coderintheworldhack100@gmail.com>
2026-05-28 15:38:28 +0530

    course controller adds the function get_courses

--- app/controllers/course_controller.py
+++ app/controllers/course_controller.py
@@ -2,16 +2,31 @@ from flask import request, jsonify
 from app.models.course import Course
 from app import db
 
+
 def create_course():
     data = request.get_json()
 
     course = Course(
         course_title=data["course_title"],
         course_fee=data["course_fee"],
-        duration_month=data["duration_month"]
+        duration_month=data["duration_month"],
     )
 
     db.session.add(course)
     db.session.commit()
 
     return jsonify({"message": "Course created"}), 201
+
+
+def get_courses():
+    courses = Course.query.all()
+    result = [
+        {
+            "id": c.id,
+            "course_title": c.course_title,
+            "course_fee": c.course_fee,
+            "duration_month": c.duration_month,
+        }
+        for c in courses
+    ]
+    return jsonify(result)
